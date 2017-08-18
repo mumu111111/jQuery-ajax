ec8fc97f22fd3a27b21b16a8592688942c61e558
mumu111111 <2646709035@qq.com>
2017-08-18 22:11:34 +0800

    jquery ajax

---
+++ .router.js
@@ -0,0 +1,34 @@
+function setRouter(app){ 
+ var router = app; 
+
+app.get('/getMore',function(req,res){
+
+
+    var news=[
+
+        '内容1',
+        '内容2',
+        '内容3',
+        '内容4',
+        '内容5',
+        '内容6',
+        '内容7',
+        '内容9',
+        '内容10'
+    ]
+
+
+    var pageIndex= req.query.page;
+    var len=3;
+
+    var retnews=news.slice(pageIndex*len,pageIndex*len+len)
+
+    res.send({
+        status: 0,
+        data: retnews
+    })
+
+
+
+})}
+ module.exports.setRouter = setRouter
